527fdb81d64e8378a8702df1400c418d1be972b1
Anthony Marin <antham22@Anthonys-MacBook-Pro.local>
2020-05-08 15:50:44 -0400

    Add folder structure and Poppins font.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="App-header">Manzana Products</h1>
+    </div>
+  );
+};
+
+export default App;
